2925092841fcbc844a79ea0a32901a1355842dd2
CoderBro2021 <87308322+CoderBro2021@users.noreply.github.com>
2023-06-12 09:26:20 +0530

    Add files via upload

---
+++ laptop.js
@@ -0,0 +1,49 @@
+img = "";
+status = "";
+object = "";
+
+
+function setup(){
+canvas = createCanvas(640,420);
+canvas.center();
+objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+document.getElementById("laptopstatus").innerHTML = "Status -> Detecting Object";
+}
+
+
+function preload(){
+img = loadImage('laptop.jpg');
+}
+
+function draw(){
+    image(img,0,0,640,420);
+
+
+    if(status != "")
+    {
+        for( i=0; i < object.length; i++)
+        {
+            document.getElementById("laptopstatus").innerHTML = "Status -> Object Detected";
+
+            fill("red");
+            percent = floor(object[i].confidence * 100);
+            text(object[i].label + "" + percent + "%",   object[i].x + 15, object[i].y +15);
+            noFill();
+            stroke("red");
+            rect(object[i].x, object[i].y , object[i].width+100, object[i].height-11);
+        }
+    }
+}
+ function modelLoaded(){
+    console.log("Model is Loaded");
+    status = true;
+    objectDetector.detect(img,gotResult);
+ }
+
+ function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+object = results;
+ }
